11197e9b09d7e7e128c5d802648ad5a5520c4666
emeraldhycient <igwezehycient86@gmail.com>
2022-09-05 14:10:13 +0100

    [update] adjusted header title position by adding empty div for when back btn isnt enabled

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -9,8 +9,12 @@ import { Title } from "react-native-paper";
 const Header = ({ showBack, navigation, title }) => {
   return (
     <View style={tw`flex flex-row justify-between items-center `}>
-      {showBack ? <HandleBack navigation={navigation} /> : null}
-      <View style={tw` mx-auto`}>
+      {showBack ? (
+        <HandleBack navigation={navigation} />
+      ) : (
+        <View style={tw`w-12`}></View>
+      )}
+      <View style={tw`mx-auto`}>
         <Title>{title && title}</Title>
       </View>
       <NotifBell />
