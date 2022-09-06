252f36ca0b961f73ae145ab67167524d50a8aad2
emeraldhycient <igwezehycient86@gmail.com>
2022-09-06 13:32:00 +0100

    [update] created onboardings files

---
+++ screens/OnboardingScreens/GetStarted.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const GetStarted = () => {
+  return (
+    <View>
+      <Text>GetStarted</Text>
+    </View>
+  );
+};
+
+export default GetStarted;
+
+const styles = StyleSheet.create({});
